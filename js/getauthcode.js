76b655e6b688e1838e91a6f7c243b87adb0eb0fa
Greg Willard <wickett06@gmail.com>
2022-10-06 18:30:01 -0400

    [oauth] use updated oauth flow for refresh_token retrieval

--- js/getauthcode.js
+++ js/getauthcode.js
@@ -1,12 +1,13 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+const OAUTH2_LOGIN_URL_BASE = 'https://accounts.google.com/o/oauth2/programmatic_auth';
+const COOKIE_NAME = 'oauth_code';
+
+chrome.runtime.onMessage.addListener(function (message) {
     if (message === "authcode") {
         console.log("message received for code")
-        chrome.cookies.get({url: "https://accounts.google.com/", name: "oauth_code"}, function (cookie) {
-            console.log("code returned", cookie)
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, cookie.value, function (response) {
-                    return true;
-                })
+        chrome.cookies.get({ url: OAUTH2_LOGIN_URL_BASE, name: COOKIE_NAME }, function (cookie) {
+            console.log("cookie returned", cookie)
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, cookie.value, () => true);
             })
         });
     }
